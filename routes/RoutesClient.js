252e119ffbf60ff0ca500a4ca0786241ea39a139
Eli Rosero <elir_94@hotmail.com>
2024-04-17 19:46:53 -0500

    clientController - Methods

--- routes/RoutesClient.js
+++ routes/RoutesClient.js
@@ -6,5 +6,10 @@ const ClientController = require('../controllers/ClientController');
 // acá van las rutas del CRUD  
 router.post('/', ClientController.addClients);
 router.get('/', ClientController.showClients);
+router.get('/:id', ClientController.showClient);
+router.delete('/:id', ClientController.deleteClients);
+router.patch('/:id', ClientController.updateClients);
+// router.put('/:id', ClientController.updateClient);
+// router.put('/:id', ClientController.upgradeClients);
 
 module.exports = router; 
